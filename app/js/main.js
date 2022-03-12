6cafb169b59a4605179925a2b71191c98c31f3dd
evgenok <popovevgen18042001@mail.ru>
2022-03-12 23:44:37 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
 $(function() {
-    
-})
+
+   $('.slider__inner').slick({
+      dots: true,
+      infinite: true,
+      speed: 1300,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+      arrows: false
+   });
+
+   var mixer = mixitup('.gallery__inner');
+});
