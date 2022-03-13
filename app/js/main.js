39eedb04bed2350d743191e869dd1fbc9d894f04
evgenok <popovevgen18042001@mail.ru>
2022-03-13 20:54:18 +0300

    added footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,17 @@
 $(function() {
+   $(".menu a").on("click", function (event) {
+      //отменяем стандартную обработку нажатия по ссылке
+      event.preventDefault();
+
+      //забираем идентификатор блока с атрибута href
+      var id = $(this).attr('href'),
+
+         //узнаем высоту от начала страницы до блока на который ссылается якорь
+         top = $(id).offset().top;
+
+      //анимируем переход на расстояние - top за 1500 мс
+      $('body,html').animate({ scrollTop: top }, 1500);
+   });
 
    $('.slider__inner').slick({
       dots: true,
@@ -7,6 +20,10 @@ $(function() {
       slidesToShow: 1,
       adaptiveHeight: true,
       arrows: false
+   });
+
+   $('.burger, .menu a').on('click', function () {
+      $('.menu__list').toggleClass('menu__list--active');
    });
 
    var mixer = mixitup('.gallery__inner');
